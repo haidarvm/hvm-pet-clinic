a4f988904e722c63ad24ab2852208e4bfda13d6f
haidarvm <haidarvm@gmail.com>
2019-06-11 23:23:49 +0700

    Initial Project Add

---
+++ src/main/java/com/haidarvm/hvmpetclinic/HvmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.haidarvm.hvmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvmPetClinicApplication.class, args);
+    }
+
+}
