1d5a0bfdff27297cde7a6f0f40bbc7b3afdb77ed
haidarvm <haidarvm@gmail.com>
2019-06-14 23:13:32 +0700

    Refactoring multi modules maven project tests updated

--- pet-clinic-web/src/test/java/com/haidarvm/hvmpetclinic/HvmPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/haidarvm/hvmpetclinic/HvmPetClinicApplicationTests.java
@@ -1,18 +1,17 @@
 package com.haidarvm.hvmpetclinic;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class HvmPetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
