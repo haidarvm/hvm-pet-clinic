e6c928869f8e7b8ccb2b3c3bcca292d14ae840bb
haidarvm <haidarvm@gmail.com>
2019-06-15 15:48:35 +0700

    Refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package com.haidarvm.hvmpetclinic.services;
 
 import com.haidarvm.hvmpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/VetService.java
@@ -2,12 +2,8 @@ package com.haidarvm.hvmpetclinic.services;
 
 import com.haidarvm.hvmpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-    Vet save(Vet vet);
 
-    Set<Vet> findAll();
 }
