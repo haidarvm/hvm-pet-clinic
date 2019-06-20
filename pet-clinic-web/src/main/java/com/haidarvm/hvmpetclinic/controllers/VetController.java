7bcb6c7c88be4dfcaea7097fa8a0641772b66f45
haidarvm <haidarvm@gmail.com>
2019-06-20 21:42:43 +0700

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/com/haidarvm/hvmpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/haidarvm/hvmpetclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("haidar", " got this man!!!");
         model.addAttribute("vets", vetService.findAll());
