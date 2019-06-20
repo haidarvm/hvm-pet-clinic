ec20d390b5a75f70ff3fd633916a3b503d2b7994
haidarvm <haidarvm@gmail.com>
2019-06-20 19:43:40 +0700

    Add Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.haidarvm.hvmpetclinic.services;
+
+import com.haidarvm.hvmpetclinic.model.Specialty;
+
+public interface SpecialtiesService  extends  CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,34 @@
+package com.haidarvm.hvmpetclinic.services.map;
+
+import com.haidarvm.hvmpetclinic.model.Specialty;
+import com.haidarvm.hvmpetclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
