50e6d6ed18b748577650943e7e6b9cd6995d7728
haidarvm <haidarvm@gmail.com>
2019-06-22 20:12:12 +0700

    Created Spring Data JPA for specialty. Closes #44

--- pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/haidarvm/hvmpetclinic/services/map/PetMapService.java
@@ -1,31 +1,31 @@
 package com.haidarvm.hvmpetclinic.services.map;
 
-import com.haidarvm.hvmpetclinic.model.PetType;
-import com.haidarvm.hvmpetclinic.services.PetTypeService;
+import com.haidarvm.hvmpetclinic.model.Pet;
+import com.haidarvm.hvmpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+public class PetMapService extends  AbstractMapService<Pet, Long> implements PetService {
 
     @Override
-    public Set<PetType> findAll() {
+    public Set<Pet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public PetType findById(Long id) {
+    public Pet findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public PetType save(PetType object) {
+    public Pet save(Pet object) {
         return super.save(object);
     }
 
     @Override
-    public void delete(PetType object) {
+    public void delete(Pet object) {
         super.delete(object);
     }
 
